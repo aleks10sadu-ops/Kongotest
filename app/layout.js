44d135349d6d8669a928e1d6cf3fa99a7f3f6654
aleks10sadu-ops <aleks10sadu@gmail.com>
2025-10-28 01:09:00 +0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Kucher&Conga - Ресторан в Дмитрове',
+  description: 'Изысканная кухня, уютная атмосфера и профессиональное обслуживание',
+}
+
+export default function RootLayout({ children }) {
+  return (
+
+      {children}
+
+  )
+}
