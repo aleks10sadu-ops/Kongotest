fd3784857d26c1dda1db044e67c67fc141b5fd3f
Kvazar277 <Kvazar_27@mail.ru>
2025-11-11 00:54:09 +0500

    Добавлен favicon для сайта

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,9 @@
 export const metadata = {
   title: 'Кучер и Конга',
   description: 'Изысканная кухня: традиции, утончённый вкус и безупречная атмосфера.',
+  icons: {
+    icon: '/icon.svg',
+  },
 };
 
 import './globals.css';
